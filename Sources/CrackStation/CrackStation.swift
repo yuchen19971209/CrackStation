7eb7194190b34aa07bcdb2c9deec8cf1ca505bae
yuchen19971209 <chenyu8@oregonstate.edu>
2022-10-25 16:27:12 -0700

    develop Init() to build hash dictionary

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -3,6 +3,23 @@ import Crypto
 public struct CrackStation {
     public private(set) var text = "Hello, World!"
 
+    private var lookupTable: [String: String] = [:]
+
+    private func loadDictionaryFromDisk() throws -> [String : String] {
+        guard let path = Bundle.module.url(forResource: "hash", withExtension: "json") else { return [:] }
+
+        let data = try Data(contentsOf: path)
+        let jsonResult = try JSONSerialization.jsonObject(with: data)
+
+        if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
+            return lookupTable
+        } else {
+            return [:]
+        }
+    }
+
+    //Run this the very first time
     public init() {
+        lookupTable = try!loadDictionaryFromDisk()
     }
 }
