331dd0e23a31086f799170ce48add72837f88908
yuchen19971209 <chenyu8@oregonstate.edu>
2022-10-25 16:30:19 -0700

    develop decrypt(shaHash:String) to return plaintext

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -22,4 +22,43 @@ public struct CrackStation {
     public init() {
         lookupTable = try!loadDictionaryFromDisk()
     }
+
+    public func decrypt(shaHash: String) -> String?{
+        //If my dictionary is empty, then throw an error
+        if lookupTable.isEmpty == true
+        {
+            //return "Dictionary is empty"
+            return "Dictionary is empty"
+        }
+        
+        return lookupTable[shaHash]
+    }
+    /*
+    func loadDictionaryFromDisk() throws -> [String : String] {
+        guard let path = Bundle.module.url(forResource: "hash", withExtension: "json") else { return [:] }
+
+        let data = try Data(contentsOf: path)
+        let jsonResult = try JSONSerialization.jsonObject(with: data)
+
+        if let lookupTable: Dictionary = jsonResult as? Dictionary<String, String> {
+            return lookupTable
+        } else {
+            return [:]
+        }
+    }
+
+    public func crack(password: String) throws -> String?{
+  
+        //When
+        let lookupTable = try loadDictionaryFromDisk()
+
+        //If my dictionary is empty, then throw an error
+        if lookupTable.isEmpty == true
+        {
+            //return "Dictionary is empty"
+            return "Dictionary is empty"
+        }
+        
+        return lookupTable[password]
+    }*/
 }
