7cf12591083de6731a2973b16bc427163b5fcbe7
yuchen19971209 <chenyu8@oregonstate.edu>
2022-11-17 17:19:43 -0800

    revise comment

--- Tests/CrackStationTests/CrackStationTests.swift
+++ Tests/CrackStationTests/CrackStationTests.swift
@@ -12,11 +12,10 @@ final class CrackStationTests: XCTestCase {
         let CrackLibrary = CrackStation()
 
         //When
-        let ans = CrackLibrary.decrypt(shaHash: "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")
-        //print(ans)
+        let password = CrackLibrary.decrypt(shaHash: "86f7e437faa5a7fce15d1ddcb9eaeaea377667b8")
 
         //Then
-        XCTAssertNotNil(ans)
+        XCTAssertNotNil(password)
     }
 
     //(SHA1) Unit test suppose to give wrong string e.g. over one character(Hello)
@@ -25,11 +24,10 @@ final class CrackStationTests: XCTestCase {
         let CrackLibrary = CrackStation()
 
         //When
-        let ans = CrackLibrary.decrypt(shaHash: "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0")
-        //print(ans)
+        let password = CrackLibrary.decrypt(shaHash: "f7ff9e8b7bb2e09b70935a5d785e0cc5d9d0abf0")
 
         //Then
-        XCTAssertNil(ans)
+        XCTAssertNil(password)
     }
 
     //(SHA1) Unit test hash mapping get correct value
@@ -38,12 +36,11 @@ final class CrackStationTests: XCTestCase {
         let CrackLibrary = CrackStation()
 
         //When
-        let ans = CrackLibrary.decrypt(shaHash: "909f99a779adb66a76fc53ab56c7dd1caf35d0fd")
-        //print(ans)
+        let password = CrackLibrary.decrypt(shaHash: "909f99a779adb66a76fc53ab56c7dd1caf35d0fd")
 
         //Then
-        XCTAssertNotNil(ans)
-        XCTAssert(ans == "Z")
+        XCTAssertNotNil(password)
+        XCTAssert(password == "Z")
     }
 
     //(SHA2) Unit test getting correct mapping value
@@ -52,11 +49,10 @@ final class CrackStationTests: XCTestCase {
         let CrackLibrary = CrackStation()
         
         //When
-        let answer = CrackLibrary.decrypt(shaHash: "b8eb0368512d7f2aa3384b66a4f0c05335c6bd58db48325d3a39c86d7fc7974a")
-        //print(answer)
+        let password = CrackLibrary.decrypt(shaHash: "b8eb0368512d7f2aa3384b66a4f0c05335c6bd58db48325d3a39c86d7fc7974a")
 
         //Then
-        XCTAssertEqual(answer, "2!")
+        XCTAssertEqual(password, "2!")
     }
 
     //(SHA1) Unit test getting correct mapping value
@@ -65,10 +61,9 @@ final class CrackStationTests: XCTestCase {
         let CrackLibrary = CrackStation()
         
         //When
-        let answer = CrackLibrary.decrypt(shaHash: "3f6d6a8699144a0028a6f47b26a3c6f70306023f")
-        //print(answer)
+        let password = CrackLibrary.decrypt(shaHash: "3f6d6a8699144a0028a6f47b26a3c6f70306023f")
 
         //Then
-        XCTAssertEqual(answer, "Tin")
+        XCTAssertEqual(password, "Tin")
     }
 }
